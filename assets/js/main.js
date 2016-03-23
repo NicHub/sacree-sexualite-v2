19f13e3deda98ef46ad7306884b9b9c1e3e4b47f
NicHub <nicolas.jeanmonod@gmail.com>
2016-03-23 22:40:41 +0100

    cleanup main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,28 +6,25 @@ $( document ).ready( function() {
 
 
 
+  // Navigation page suivante ou précédente avec les flèches
+  // gauche-droite du clavier.
   ( function() {
-    // Navigation page suivante ou précédente avec les flèches
-    // gauche-droite du clavier.
     Mousetrap.bind( 'left',  function(e) { navigate_prev_page(); });
     Mousetrap.bind( 'right', function(e) { navigate_next_page(); });
     function navigate_next_page() {
        var next_page = $( ".bouton-suiv" ).first().attr( "href" );
-       // console.log( next_page );
        window.location.href = next_page;
     }
     function navigate_prev_page() {
        var prev_page = $( ".bouton-prec" ).first().attr( "href" );
-       // console.log( prev_page );
        window.location.href = prev_page;
     }
   })();
 
 
 
-    // console-log ('form-submit-button');
+  // Formulaire de contact
   ( function() {
-    // Formulaire
     var $contactForm = $( '#contact-form' );
     $contactForm.submit( function(e) {
       e.preventDefault();
@@ -38,9 +35,6 @@ $( document ).ready( function() {
       var form_email_length   = $( '#form-email' ).val().length;
       var form_nom_length     = $( '#form-nom' ).val().length;
       var form_message_length = $( '#form-message' ).val().length;
-      console.log( form_email_length );
-      console.log( form_nom_length );
-      console.log( form_message_length );
       if( form_email_length == 0 || form_nom_length == 0 || form_message_length == 0 ) {
         $form_status.html( '<div class="alert alert-danger" role="alert"><span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span> Veuillez remplir tous les champs <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span></div>' );
         return;
