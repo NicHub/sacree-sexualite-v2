81b8c7670c80be644ff66659547ab1e43a02a46c
NicHub <nicolas.jeanmonod@gmail.com>
2016-03-31 15:24:29 +0200

    corrigé bug swipe souris

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,33 +24,30 @@ $( document ).ready( function() {
   // Navigation page suivante ou précédente avec les flèches
   // gauche-droite du clavier (Mousetrap, https://craig.is/killing/mice)
   // et le balayage gauche-droite de l’écran (jQuery Mobile, https://jquerymobile.com/).
+  // !! Il faut discriminer les écrans tactiles des souris.
+  // !! L’évènement “swipe” est activé lors d’un “pinch” si les dimensions de
+  // la page sont fixées dans la balise <meta> "viewport". Il faut donc laisser
+  // la possibilité de zoomer avec deux doigts.
   ( function() {
-    Mousetrap.bind( 'left',       function( e ) { navigate_prev_page( e ); });
-    Mousetrap.bind( 'esc',        function( e ) { navigate_home_page( e ); });
-    Mousetrap.bind( 'right',      function( e ) { navigate_next_page( e ); });
+
+    var navigate_to_page = function( e, button_class ) {
+      var target_page = $( button_class ).first().attr( 'href' );
+      window.location.href = target_page;
+    }
+
+    Mousetrap.bind( 'left',       function( e ) { navigate_to_page( e, '.bouton-prec'    ); });
+    Mousetrap.bind( 'esc',        function( e ) { navigate_to_page( e, '.bouton-accueil' ); });
+    Mousetrap.bind( 'right',      function( e ) { navigate_to_page( e, '.bouton-suiv'    ); });
 
     $( 'body' ).on( 'mousedown',  function( e ) { disable_swipe( e ); });
     $( 'body' ).on( 'touchstart', function( e ) { enable_swipe( e );  });
 
     function disable_swipe( e ) {
-      $( 'body' ).off( 'swiperight' );
-      $( 'body' ).off( 'swipeleft'  );
+      $( 'body' ).off( 'swiperight swipeleft' );
     }
     function enable_swipe( e ) {
-      $( 'body' ).on( 'swiperight', function( e ) { navigate_prev_page( e ); });
-      $( 'body' ).on( 'swipeleft',  function( e ) { navigate_next_page( e ); });
-    }
-    function navigate_next_page( e ) {
-      var target_page = $( '.bouton-suiv' ).first().attr( 'href' );
-      window.location.href = target_page;
-    }
-    function navigate_home_page( e ) {
-      var target_page = $( '.bouton-accueil' ).first().attr( 'href' );
-      window.location.href = target_page;
-    }
-    function navigate_prev_page( e ) {
-      var target_page = $( '.bouton-prec' ).first().attr( 'href' );
-      window.location.href = target_page;
+      $( 'body' ).on( 'swiperight', function( e ) { navigate_to_page( e, '.bouton-prec' ); });
+      $( 'body' ).on( 'swipeleft',  function( e ) { navigate_to_page( e, '.bouton-suiv' ); });
     }
   })();
 
